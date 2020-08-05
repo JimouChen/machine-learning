d79b7992ed25f837e96d01244f4eed093a7db37f
jmchen <jmc1625645320@gmail.com>
2020-08-05 13:22:17 +0800

    sklearn多项式拟合回归

---
+++ machine_learning/回归/多项式回归/sklearn_多项式回归.py
@@ -0,0 +1,47 @@
+"""
+# @Time    :  2020/8/5
+# @Author  :  Jimou Chen
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures  # 生成多项式用的
+
+# 读取数据
+data = np.genfromtxt(r'job.csv', delimiter=',')
+x_data = data[1:, 1]
+y_data = data[1:, -1]
+
+# plt.scatter(x_data, y_data)
+# plt.show()
+
+# 转换为二维数据
+x_data = x_data[:, np.newaxis]  # 或者x_data = data[1:, 1, np.newaxis]
+y_data = y_data[:, np.newaxis]  # 或者y_data = data[1:, -1, np.newaxis]
+
+# 建模一元线性回归拟合
+# model = LinearRegression()
+# model.fit(x_data, y_data)
+
+# 画线性回归线图看看效果
+# plt.plot(x_data, y_data, 'b.')
+# plt.plot(x_data, model.predict(x_data), 'r')
+# plt.show()
+
+# 定义多项式回归，degree的值可以调节多项式的特征
+# degree = n，相当于n次方拟合
+poly = PolynomialFeatures(degree=5)
+# 特征处理
+x_poly = poly.fit_transform(x_data)
+# print(x_poly)
+# 定义回归模型,并拟合
+lin_reg = LinearRegression()
+lin_reg.fit(x_poly, y_data)
+
+# 画图
+plt.plot(x_data, y_data, 'b.')
+plt.plot(x_data, lin_reg.predict(x_poly), 'r')  # predict 传的是x_poly,是处理后的数据
+plt.title('Polynomial Regression')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
