311d5748215ffa71f4610343095cf2f5de9b0e34
jimouchen <jmc1625645320@gmail.com>
2020-08-12 01:12:51 +0800

    decisionTree 线性/非线性二分类

---
+++ machine_learning/决策树/decisionTree_线性二分类.py
@@ -0,0 +1,40 @@
+"""
+# @Time    :  2020/8/12
+# @Author  :  Jimou Chen
+"""
+import numpy as np
+from sklearn import tree
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('LR-testSet.csv', delimiter=',')
+x_data = data[:, :-1]
+y_data = data[:, -1]
+
+plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data)
+plt.show()
+
+model = tree.DecisionTreeClassifier()
+model.fit(x_data, y_data)
+
+print(y_data)
+pred = model.predict(x_data)
+print(pred)
+
+print(classification_report(y_data, pred))
+
+# 获取数据值所在的范围
+x_min, x_max = x_data[:, 0].min() - 1, x_data[:, 0].max() + 1
+y_min, y_max = x_data[:, 1].min() - 1, x_data[:, 1].max() + 1
+
+# 生成网格矩阵
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                     np.arange(y_min, y_max, 0.02))
+
+z = model.predict(np.c_[xx.ravel(), yy.ravel()])  # ravel与flatten类似，多维数据转一维。flatten不会改变原始数据，ravel会改变原始数据
+z = z.reshape(xx.shape)
+# 等高线图
+cs = plt.contourf(xx, yy, z)
+# 样本散点图
+plt.scatter(x_data[:, 0], x_data[:, 1], c=y_data)
+plt.show()
