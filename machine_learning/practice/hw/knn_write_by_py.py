7a3b6e2aa6f0eb004f21075b8386e285c33f754d
jimouchen <jmc1625645320@gmail.com>
2020-09-26 09:06:47 +0800

    knn test

---
+++ machine_learning/practice/hw/knn_write_by_py.py
@@ -0,0 +1,48 @@
+"""
+# @Time    :  2020/9/26
+# @Author  :  Jimou Chen
+"""
+import operator
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def kNN(x_test, x_data, y_data, k):
+    # 计算多少个样本
+    x_data_size = x_data.shape[0]
+    # 复制x_test，复制行x_data_size次，复制列1次
+    x_test_copy = np.tile(x_test, (x_data_size, 1))
+    # 计算x_test与每个样本的差值 ,计算差值平方
+    sq_diff_mat = (x_test_copy - x_data) ** 2
+    # 对差值求和,再开方,得到每个样本与测试样本的距离
+    distance = (sq_diff_mat.sum(axis=1)) ** 0.5
+    # 对上面得到的距离索引进行从小到大排序
+    sorted_distance = distance.argsort()
+    # 进行分类，把分类结果按多到少放到一个字典
+    class_count = {}
+    for i in range(k):
+        # 获取标签
+        label = y_data[sorted_distance[i]]
+        # 统计标签数量，如果不存在该标签就把数量置0加1
+        class_count[label] = class_count.get(label, 0) + 1
+    # 将分类结果按数量按多到少排序
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reverse=True)
+
+    # 返回数量最多的类别
+    return sorted_class_count[0][0]
+
+
+if __name__ == '__main__':
+    # 找几个点试试
+    x_train = np.array([[1, 1], [2, 1.3], [3, 1.5], [4, 0.8], [5, 5], [6, 5.5], [7, 4.5]])
+    y_train = np.array([0, 0, 0, 0, 1, 1, 1])
+    # 测试点
+    x_test = np.array([[5, 4.5]])
+
+    # 画出散点图
+    plt.scatter(x_test[0][0], x_test[0][1], c='r')
+    plt.scatter(x_train[:, 0], x_train[0:, 1], c=y_train)
+    plt.show()
+
+    class_test = (kNN(x_test[0], x_train, y_train, 3))
+    print('预测的类别是：', class_test)
