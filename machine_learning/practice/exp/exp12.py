d101feaca6f67e3ab68a69abc862e06860a36a01
jimouchen <jmc1625645320@gmail.com>
2020-10-17 22:05:21 +0800

    pcajia多分类

--- machine_learning/practice/exp/exp12.py
+++ machine_learning/practice/exp/exp12.py
@@ -2,63 +2,63 @@
 # @Time    :  2020/10/15
 # @Author  :  Jimou Chen
 """
-from sklearn.linear_model import Lasso, LinearRegression, Ridge
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
-def product_num(row, col, theta, mean):
-    data = np.random.randn(row, col) * theta + mean
-    x_data = data[:, 0]
-    y_data = data[:, 1]
-    return x_data, y_data, data
+# 定义一个产生二维数据的函数，通过改变方差和均值来改变位置
+def product_num(num, theta_x, mean_x, theta_y, mean_y):
+    x = np.random.randn(num, 1) * theta_x + mean_x
+    y = np.random.randn(num, 1) * theta_y + mean_y
+    data = np.concatenate((x, y), axis=1)
 
-#
-# def handle_pred_res(pred):
-#
-#     for i in range(len(pred)):
-#         pred[i] = int(round(pred[i]))
-#
-#     return pred
+    return x, y, data
 
 
 if __name__ == '__main__':
-    x1, y1, data1 = product_num(300, 2, 30, -30)
-    x2, y2, data2 = product_num(300, 2, 10, 20)
-    x3, y3, data3 = product_num(300, 2, 20, 50)
-    x4, y4, data4 = product_num(300, 2, 20, 110)
+    # 生成数据特征
+    x1, y1, data1 = product_num(400, 1, 0, 1, 0)
+    x2, y2, data2 = product_num(400, 1, 1, 1.5, 4)
+    x3, y3, data3 = product_num(400, 2, 8, 1, 5)
+    x4, y4, data4 = product_num(400, 0.5, 3, 0.5, 3)
 
-    label1 = np.zeros(300, dtype=int)
-    label2 = np.zeros(300, dtype=int) + 1
-    label3 = np.zeros(300, dtype=int) + 2
-    label4 = np.zeros(300, dtype=int) + 3
+    # 设置标签，4个类
+    label1 = np.zeros(400, dtype=int)
+    label2 = np.zeros(400, dtype=int) + 1
+    label3 = np.zeros(400, dtype=int) + 2
+    label4 = np.zeros(400, dtype=int) + 3
 
+    # 画图
+    plt.scatter(x1, y1, c='b', marker='^', label='class1')
+    plt.scatter(x2, y2, c='r', marker='v', label='class2')
+    plt.scatter(x3, y3, c='y', marker='<', label='class3')
+    plt.scatter(x4, y4, c='g', marker='>', label='class4')
+    plt.legend()
+    plt.show()
+
+    # 合并特征和标签
     data = np.concatenate((data1, data2, data3, data4), axis=0)
     label = np.concatenate((label1, label2, label3, label4), axis=0)
 
-    plt.scatter(x1, y1, c='r')
-    plt.scatter(x2, y2, c='b')
-    plt.scatter(x3, y3, c='g')
-    plt.scatter(x4, y4, c='y')
-    plt.show()
-
+    # 切分数据集
     x_train, x_test, y_train, y_test = train_test_split(data, label)
 
+    # 建模拟合
     model = LinearRegression()
-    # model = Lasso()
     # model = LogisticRegression()
     model.fit(x_train, y_train)
+
+    # 进行预测类别
     pred1 = model.predict(x_test)
     prediction = model.predict(data)
-    prediction = np.array(prediction, dtype=int)
+    # 由于调用了线性回归分类器，所以得到的结果是连续的，所以转换成离散的整数
+    prediction = np.trunc(np.array(prediction)).astype(int)
 
+    # 显示数据
     np.set_printoptions(threshold=10000)
     print(prediction)
-    # prediction = handle_pred_res(prediction)
-    # print(prediction)
-    # print(prediction.shape)
 
+    # 准确率
     print(model.score(x_test, y_test))
